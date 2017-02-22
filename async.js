13ab5f0f661cdfb885ccd7e842c9d23a47fb4d08
Jason Yu <ycm.jason@gmail.com>
2017-02-22 12:17:22 +0000

    added comments to explain in more detail

--- async.js
+++ async.js
@@ -6,20 +6,35 @@ function getSomething(){
   });
 }
 
+/* `getAndPrint` and `getAndPrintAsync` are equivalent */
 function getAndPrint(){
   /* ES6, without await/async */
   var s = getSomething();
   s.then((s) => console.log("ES6: " + s));
+  // anything afterwards are still executed even when getSomething's promise hasn't been resolved
+  console.log("ES6: getSomething is still going on but this line is printed");
 }
 
 async function getAndPrintAsync(){
   /* ES7, with await and async */
-  var s = getSomething();
   var s = await getSomething();
+  // anything afterwards is after getSomething's promise resolves
+  console.log("ES7: getSomething is finish.");
   console.log("ES7: " + s);
 }
 
 getAndPrint();
 getAndPrintAsync();
+
 setTimeout(()=>console.log('hi2'), 0);
 console.log('hi');
+
+/* Output:
+ *
+ * ES6: getSomething is still going on but this line is printed
+ * hi
+ * hi2
+ * ES6: something
+ * ES7: getSomething is finish.
+ * ES7: something
+ */
